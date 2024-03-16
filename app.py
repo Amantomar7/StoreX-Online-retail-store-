41992875acb61a39e9492b038d0a7ac287dbf237
Amantomar7 <aman22061@iiitd.ac.in>
2024-03-16 12:19:28 +0530

    first commit

--- app.py
+++ app.py
@@ -1,13 +1,124 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+import mysql.connector
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Aman",
+        database="schemaall"
+    )
 
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
 
+#This is homepage route
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
+
+@app.route('/register', methods = ['GET', 'POST'])
+def UserType():
+    if(request.method == 'POST'):
+        User = request.form['RegisterRadio']
+        if(User == 'customer'):
+            return redirect('/registerCustomer')
+        elif (User == 'supplier'):
+            return redirect('/registerSupplier')
+        else :
+            return redirect('/registerDeliveryAgent')
+    return render_template('UserType.html')
+
+# This is login page
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if(request.method == 'POST'):
+        User = request.form['User Type']
+        Email = request.form['Email']
+        Password = request.form['Password']
+        conn = get_connection()
+        cursor = conn.cursor()
+        if(User == 'customer'):
+            cursor.execute("SELECT * FROM Customer WHERE Email = %s AND Hash_Password = %s", (Email, Password))
+        elif (User == 'delivery Agent'):
+            cursor.execute("SELECT * FROM DeliveryAgent WHERE Email = %s AND Hash_Password = %s", (Email, Password))
+        else :
+            cursor.execute("SELECT * FROM Supplier WHERE Email = %s AND Hash_Password = %s", (Email, Password))
+            
+        user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if user:
+            return redirect('/')
+        else :
+            return "wrong details"
+    return render_template('login.html')
+
+
+# This is register Page
+@app.route('/registerCustomer', methods = ['GET', 'POST'])
+def registerCustomer():
+    if(request.method == 'POST'):
+        First_Name = request.form['First Name']
+        Last_Name = request.form['Last Name']
+        Age = request.form['Age']
+        Email = request.form['Email']
+        Phone = request.form['Phone']
+        Address = request.form['Address']
+        Pincode = request.form['Pincode']
+        Password = request.form['Password']
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO Customer (FirstName, LastName, Email, Phone, Age, Hash_Password, Address, State_Pincode) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)''', (First_Name, Last_Name, Email, Phone, Age, Password, Address, Pincode))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect('/') #return to main site page
+    return render_template('registerCustomer.html')
+
+# This is register Page
+@app.route('/registerSupplier', methods = ['GET', 'POST'])
+def registerSupplier():
+    if(request.method == 'POST'):
+        Name = request.form['Name']
+        Email = request.form['Email']
+        Phone = request.form['Phone']
+        Address = request.form['Address']
+        Pincode = request.form['Pincode']
+        Password = request.form['Password']
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO Supplier (SupplierName, Email, Phone, Hash_Password, Address, State_Pincode) VALUES (%s, %s, %s, %s, %s, %s)''', (Name, Email, Phone, Password, Address, Pincode))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect('/')
+    return render_template('registerSupplier.html')
+
+# This is register Page
+@app.route('/registerDeliveryAgent', methods = ['GET', 'POST'])
+def registerDeliveryAgent():
+    if(request.method == 'POST'):
+        First_Name = request.form['First Name']
+        Last_Name = request.form['Last Name']
+        Email = request.form['Email']
+        Phone = request.form['Phone']
+        Address = request.form['Address']
+        Pincode = request.form['Pincode']
+        Password = request.form['Password']
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO DeliveryAgent (FirstName, LastName, Email, Phone, Hash_Password, Address, State_Pincode) VALUES (%s, %s, %s, %s, %s, %s, %s)''', (First_Name, Last_Name, Email, Phone, Password, Address, Pincode))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect('/')
+    return render_template('registerDeliveryAgent.html')
+
 
 if __name__ == "__main__":
     app.run(debug = True)
